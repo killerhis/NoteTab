bcd92bfd97148bbdc632504ea4e72097f43da414
Hicham Chourak <hichamchourak@Hichams-Mac.local>
2014-07-09 15:46:26 +0200

    Initial Commit

---
+++ NotesApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NotesApp
+//
+//  Created by Hicham Chourak on 09/07/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NotesApp/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  NotesApp
+//
+//  Created by Hicham Chourak on 09/07/14.
+//  Copyright (c) 2014 Hicham Chourak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
